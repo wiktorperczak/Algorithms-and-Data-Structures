3a60ed07c3cf4c44d128cb0644ea0ed4c04d19fb
wiktor <wiktor.prczk@gmail.com>
2023-04-26 17:41:02 +0200

    euler cycle

--- graphAlgorithms/eulerCycle.cpp
+++ graphAlgorithms/eulerCycle.cpp
@@ -3,35 +3,35 @@ using namespace std;
 
 const int n = 7;
 int odw[n+1][n+1] = {0};
-vector<vector<int>>g(n+1);
-vector<int>stos;
+vector<vector<int>> g(n+1);
+vector<int> stos;
 
-void dfs(int x) {
-    for(int i=0; i<(int)g[x].size(); i++) {
-        cout<<"RAZ: "<<g[x][i]<<" "<<odw[x][g[x][i]]<<"\n";
+void dfs(int x){
+    for(int i = 0; i < (int) g[x].size(); i++) {
+        cout << "RAZ: " << g[x][i]<<" " << odw[x][g[x][i]] << "\n";
         if(!odw[x][g[x][i]]) {
             odw[x][g[x][i]] = 1;
             odw[g[x][i]][x] = 1;
             dfs(g[x][i]);
-            cout<<"DWA: "<<odw[x][g[x][i]]<<" "<<g[x][i]<<"\n";
+            cout << "DWA: " << odw[x][g[x][i]] << " " << g[x][i] << "\n";
             stos.push_back(g[x][i]);
         }
     }
 }
 
-int main() {
-    for(int i=1; i<=n; i++) {
+int main(){
+    for(int i = 1; i <= n; i++) {
         int ile;
-        cin>>ile;
-        for(int j=0; j<ile; j++) {
+        cin >> ile;
+        for(int j = 0; j < ile; j++) {
             int a;
-            cin>>a;
+            cin >> a;
             g[i].push_back(a);
         }
     }
     dfs(1);
-    cout<<"1 ";
-    for(int i=(int)stos.size()-1; i>=0; i--)
-        cout<<stos[i]<<" ";
+    cout << "1 ";
+    for(int i = (int) stos.size() - 1; i >= 0; i--)
+        cout << stos[i] << " ";
     return 0;
 }
